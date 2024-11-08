616c68b4f41c9308bf734921cd40f682cb85196e
siam3h <philosiama@gmail.com>
2024-11-08 23:58:03 +0300

    Update the vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    base: '/liveinnairobi/',
+    base: '/',  
     plugins: [vue()],
     resolve: {
-        alias: { '@': '/src' },
+        alias: {
+            '@': '/src',
+        },
     },
     server: {
         proxy: {
@@ -27,5 +29,4 @@ export default defineConfig({
             },
         },
     },
-
 });
