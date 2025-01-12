40b394b4fe2c2b564b23a4bf11c4d9ae7ba7ed1f
siam3h <philosiama@gmail.com>
2025-01-12 21:46:52 +0300

    update apiClient

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -5,11 +5,26 @@ const apiClient = axios.create({
   baseURL: 'https://liveinnbo-backend.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
+    'X-CSRFToken': getCookie('csrftoken')  // Add this line
   },
-  //withCredentials: true,
-  xsrfHeaderName: 'X-CSRFToken', 
-  xsrfCookieName: 'csrftoken',     
+  withCredentials: true     
 });
+
+// Add this function to get CSRF cookie
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
 
 export default {
     // Blog APIs
@@ -52,7 +67,7 @@ export default {
     
     // Auth APIs
     authSignUp(data) {
-        return apiClient.post('/api/v1/users/auth/signup/', data);
+        return apiClient.post('/users/auth/signup/', data);
     },      
     authSignIn(){
         return apiClient.post(`/users/auth/signin/`)
