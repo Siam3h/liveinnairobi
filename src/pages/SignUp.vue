bb043c8487a3ec63d3c9d81689c1891107e681c8
siam3h <philosiama@gmail.com>
2025-01-12 05:27:48 +0300

    update sign up vue

--- src/pages/SignUp.vue
+++ src/pages/SignUp.vue
@@ -3,9 +3,8 @@
   <div>
     <h1>Sign Up</h1>
     <form @submit.prevent="signUp">
-    <input v-model="event_organizer" type="event_organizer" placeholder="Event Organiser" required />
       <input v-model="email" type="email" placeholder="Email" required />
-      <input v-model="phone" type="phone" placeholder="Phone Number" required />
+      <input v-model="phone_number" type="phone_number" placeholder="Phone Number" required />
       <input v-model="username" type="username" placeholder="username" required />
       <input v-model="password" type="password" placeholder="Password" required />
       <button type="submit">Sign Up</button>
@@ -21,14 +20,13 @@ export default {
     return {
       email: '',
       password: '',
-      phone:'',
-      event_organizer:''
+      phone_number:'',
     };
   },
   methods: {
     async signUp() {
       try {
-        const response = await apiClient.authSignUp({ email: this.email, password: this.password, phone: this.phone, event_organizer: this.event_organizer, username: this.username });
+        const response = await apiClient.authSignUp({ email: this.email, password: this.password, phone: this.phone_number, username: this.username });
         console.log('Signup successful:', response.data.message);
         this.$router.push({ name: 'user-dashboard' });
       } catch (error) {
