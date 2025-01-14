f002543c467163d6edb400225ddab78b86ffb8a5
siam3h <philosiama@gmail.com>
2025-01-14 13:43:10 +0300

    update

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -142,11 +142,7 @@
   <script>
   import Navbar from '../components/Navbar.vue';
   import Footer from '../components/Footer.vue';
-<<<<<<< HEAD
-  // import api from '../api';
-=======
   import apiClient from '../services/apiClient';
->>>>>>> 14c8a422dd0a7bf4d7012387886084444d1ef684
   
   export default {
     components: {
